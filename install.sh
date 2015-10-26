eb1ce301bbc3796e886941104c6e598de2df0b4f
Gerd Aschemann <gerd@aschemann.net>
2015-10-26 05:07:17 +0100

    Fix: Only open tty on interactive sessions

--- install.sh
+++ install.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+uid=`id -u`
+
+# The following docker flags must only be set for interactive sessions with terminal
+interactive="-it"
+tty -s || interactive=""
+
 cordova () {
-    docker run -u `id -u` -it --rm -v $PWD:/src -e 'HOME=/tmp' cordova cordova "$@"
+    docker run -u ${uid} ${interactive} --rm -v $PWD:/src -e 'HOME=/tmp' cordova cordova "$@"
 }
 
 test -d platforms || mkdir platforms
