cd00cd68a77edfc31001ed30b46d47bd7d5955c7
Gerd Aschemann <gerd@aschemann.net>
2015-10-26 05:01:55 +0100

    Re-create "platforms" if necessary

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+
 cordova () {
     docker run -u `id -u` -it --rm -v $PWD:/src -e 'HOME=/tmp' cordova cordova "$@"
 }
 
+test -d platforms || mkdir platforms
 test -d platforms/android || cordova platform add android
 cp -p build.gradle platforms/android
 
